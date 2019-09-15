3e60293688f16ea622076168b1652eb94434c112
zhangwen <zhangwenit@126.com>
2019-09-15 16:00:37 +0800

    second commit

---
+++ src/test/java/com/zhangwenit/rabbitmq/producer/demo/RabbitmqProducerDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhangwenit.rabbitmq.producer.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RabbitmqProducerDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
